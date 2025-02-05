2b62f32548a5e8632356a52737d242868406e4ba
datrujil <91101091+datrujil@users.noreply.github.com>
2025-02-05 00:06:24 -0800

    Added Register and Logout Views

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from server.views import LoginView, get_csrf_token
+from server.views import LoginView, LogoutView, RegisterView, get_csrf_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/csrf/", get_csrf_token, name="csrf_token"),  # ✅ Fetch CSRF token before login
     path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
 ]

49d261533e2bef7c3bb29306cfa2847f672a3463
datrujil <91101091+datrujil@users.noreply.github.com>
2025-02-12 00:35:30 -0800

    Added new checkin URLs and APIs
    **We may need to refactor this later**

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from server.views import LoginView, LogoutView, RegisterView, get_csrf_token
+from server.views import LoginView, LogoutView, RegisterView, get_csrf_token, CheckinView, check_student, available_classes, class_details, checkin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/csrf/", get_csrf_token, name="csrf_token"),  # ✅ Fetch CSRF token before login
+    path("auth/csrf/", get_csrf_token, name="csrf_token"),  # Fetch CSRF token before login
     path("auth/login/", LoginView.as_view(), name="login"),
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
+    path("auth/checkin/", CheckinView.as_view(), name="checkin"),
+    path("api/check_student/", check_student, name="check_student"),
+    path("api/available_classes/", available_classes, name="available_classes"),
+    path("api/class_details/<int:classID>/", class_details, name="class_details"),
+    path("api/checkin/", checkin, name="checkin"),
 ]

11dfcb919ff95b89e5fbd40f808df70e0efbb358
Baobaos <bchang758@gmail.com>
2025-02-18 12:06:48 -0800

    updated calendar header so that it matches prototype. created new calendar.css file

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -12,12 +12,37 @@ export default function Calendar() {
       plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
       initialView="timeGridWeek"
       headerToolbar={{
-        start: "timeGridDay,timeGridWeek,dayGridMonth",
-        center: "prev,title,next",
-        // end: "prev, "
-      }}
+        start: "timeGridDay,timeGridWeek,dayGridMonth today",
+        center: "prev title next",
+        end: "addClassButton",
+        }}
+        customButtons={{
+          addClassButton: {
+            text: "Add Class",
+            click: function () {
+              alert("Add Class button clicked!"); // in the works
+            },
+          },
+        }}
+        titleFormat={{ year: "numeric", month: "long" }}
       height = {"90vh"}
     />
+    <style>
+        {`
+          .fc-toolbar.fc-header-toolbar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-wrap: nowrap;
+          }
+          
+          .fc-toolbar-chunk {
+            display: flex !important;
+            align-items: center !important;
+            gap: 10px;
+          }
+        `}
+      </style>
     </div>
   )
 }

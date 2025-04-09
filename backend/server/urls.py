f44ac2ad8b7af608c627bab459cca956f28d4430
Derek Adlai Trujillo <derektrujil@gmail.com>
2025-04-09 15:46:25 -0700

    Added checkin selection page

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import RedirectView
-from server.views import LoginView, LogoutView, RegisterView, get_csrf_token, CheckinView, check_student, available_classes_today, class_details, checkin
+from server.views import LoginView, LogoutView, RegisterView, get_csrf_token, CheckinView, MemberSignupView, GuestCheckinView, CheckinSelectionView, check_student, available_classes_today, class_details, checkin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,7 +9,10 @@ urlpatterns = [
     path("auth/login/", LoginView.as_view(), name="login"),
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
+    path("auth/checkin-selection/", CheckinSelectionView.as_view(), name="checkin_selection"),
     path("auth/checkin/", CheckinView.as_view(), name="checkin"),
+    path("auth/member-signup/", MemberSignupView.as_view(), name="member_signup"),
+    path("auth/guest-checkin/", GuestCheckinView.as_view(), name="guest_checkin"),
     path("api/check_student/", check_student, name="check_student"),
     path("api/available_classes_today/", available_classes_today, name="available_classes_today"),
     path("api/class_details/<int:classID>/", class_details, name="class_details"),
